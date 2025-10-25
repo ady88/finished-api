308ac4c825ebc235ea8a7dcf68aa239b8db5c0c0
adrian <ady8_8@yahoo.com>
2025-10-25 18:25:13 +0300

    remove deprecated declaration

--- src/main/java/com/adrian/finished/model/abilities/AbilityExecutor.java
+++ src/main/java/com/adrian/finished/model/abilities/AbilityExecutor.java
@@ -7,12 +7,4 @@ package com.adrian.finished.model.abilities;
 @FunctionalInterface
 public interface AbilityExecutor {
     com.adrian.finished.model.GameState apply(AbilityContext context);
-
-    /**
-     * Use AbilityExecutors.withListeners(delegate, listeners) instead.
-     */
-    @Deprecated(forRemoval = false, since = "1.0")
-    static AbilityExecutor withListeners(AbilityExecutor delegate, AbilityExecutionListener... listeners) {
-        return AbilityExecutors.withListeners(delegate, listeners);
-    }
 }
