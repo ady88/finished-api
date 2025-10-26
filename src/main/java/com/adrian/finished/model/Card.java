fa7b7e34a4e91a958ae0b0b66ae81bc837305d02
adrian <ady8_8@yahoo.com>
2025-10-26 20:32:20 +0200

    updated Card record

--- src/main/java/com/adrian/finished/model/Card.java
+++ src/main/java/com/adrian/finished/model/Card.java
@@ -8,8 +8,10 @@ package com.adrian.finished.model;
  * - abilitiesTriggered: how many times abilities on this card were triggered this turn.
  * - maxAbilities: the maximum number of times abilities on this card may be triggered this turn.
  *   Example: card 47 (drawOne3x) can be triggered up to 3 times; most others are 0 or 1.
+ * - fromDrawStack: true if the card entered the present area from the draw stack on this turn,
+ *   false if it came from past or future areas. This determines whether takeCandy should be applied.
  */
-public record Card(int number, int abilitiesTriggered, int maxAbilities) {
+public record Card(int number, int abilitiesTriggered, int maxAbilities, boolean fromDrawStack) {
     public Card {
         if (number < 1 || number > 48) {
             throw new IllegalArgumentException("Card number must be between 1 and 48, got: " + number);
@@ -23,5 +25,13 @@ public record Card(int number, int abilitiesTriggered, int maxAbilities) {
         if (abilitiesTriggered > maxAbilities) {
             throw new IllegalArgumentException("abilitiesTriggered (" + abilitiesTriggered + ") cannot exceed maxAbilities (" + maxAbilities + ")");
         }
+    }
+
+    /**
+     * Convenience constructor that defaults fromDrawStack to true.
+     * Used when creating cards that are known to come from the draw stack.
+     */
+    public Card(int number, int abilitiesTriggered, int maxAbilities) {
+        this(number, abilitiesTriggered, maxAbilities, true);
     }
 }
